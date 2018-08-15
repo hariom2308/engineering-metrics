0e749c9dbf38a9516d355f1f21fd284ac0b3cc3d
hariom2308 <hom@babbel.com>
2018-08-15 15:31:18 +0200

    Add setTimeout() to delay the requests per sec

--- index.js
+++ index.js
@@ -9,29 +9,32 @@ babbel
   .then(repos => {
     const repoArray = repos.data;
     for (let i = 0; i < repoArray.length; i++) {
-      const repoName = repoArray[i].name;
-      let repo = gh.getRepo("lessonnine", repoName);
-      repo
-        .listPullRequests({})
-        .then(res => {
-          const numberOfOpenPullRequests = res.data.length;
-          if (numberOfOpenPullRequests != 0) {
-            for (let j = 0; j < numberOfOpenPullRequests; j++) {
-              const pullReq = res.data[j];
-              const pullRequestData = [
-                res.headers.date,
-                repoName,
-                pullReq.html_url,
-                pullReq.title,
-                pullReq.created_at,
-                pullReq.updated_at,
-                pullReq.user.login
-              ];
-              console.log(pullRequestData.join(";"));
+      setTimeout( function timer(){
+        const repoName = repoArray[i].name;
+        let repo = gh.getRepo("lessonnine", repoName);
+        repo
+          .listPullRequests({})
+          .then(res => {
+            const numberOfOpenPullRequests = res.data.length;
+            if (numberOfOpenPullRequests != 0) {
+              for (let j = 0; j < numberOfOpenPullRequests; j++) {
+                const pullReq = res.data[j];
+                const pullRequestData = [
+                  res.headers.date,
+                  repoName,
+                  pullReq.html_url,
+                  pullReq.title,
+                  pullReq.created_at,
+                  pullReq.updated_at,
+                  pullReq.user.login
+                ];
+                console.log(pullRequestData.join(";"));
+              }
             }
-          }
-        })
-        .catch(err => console.log(err));
+          })
+          .catch(err => console.log(err));
+    }, i*10 );
+
     }
   })
   .catch(err => {
