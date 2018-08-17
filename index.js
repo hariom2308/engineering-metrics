a2fbb78f1031c375536003bd257bf2bd7f01e8a0
hariom2308 <hom@babbel.com>
2018-08-17 09:50:23 +0200

    Extract callback functions

--- index.js
+++ index.js
@@ -2,44 +2,55 @@ require("dotenv").config();
 const GitHub = require("github-api");
 const accessToken = process.env.ACCESS_TOKEN;
 const gh = new GitHub({ token: accessToken });
-const babbel = gh.getOrganization("lessonnine");
+const organization = "babbel";
+const babbel = gh.getOrganization(organization);
+let pullReqArr = [];
+
+const displayPullRequestsForRepo = repoName => {
+
+  let repo = gh.getRepo(organization, repoName);
+  repo
+    .listPullRequests({})
+    .then(res => {
+      const numberOfOpenPullRequests = res.data.length;
+      if (numberOfOpenPullRequests != 0) {
+        for (let j = 0; j < numberOfOpenPullRequests; j++) {
+          const pullReq = res.data[j];
+          const pullRequestData = [
+            res.headers.date,
+            repoName,
+            pullReq.html_url,
+            pullReq.title,
+            pullReq.created_at,
+            pullReq.updated_at,
+            pullReq.user.login
+          ];
+          pullReqArr.push(pullRequestData.join("; "));
+        }
+      }
+      console.log(pullReqArr.join("\n"));
+    })
+    .catch(err => {
+       console.log("Forbidden: listPullRequests() didnot work");
+       process.exit(1);
+    });
+}
+
+const displayPullRequestsForOrg = repos => {
+  const repoArray = repos.data;
+  for (let i = 0; i < repoArray.length; i++) {
+    const repoName = repoArray[i].name;
+    const delay = i * 10;
+    // setTimeout is implemented to avoid concurrent api
+    // req so that it doesn't exceed the rate limit of the Github api
+    setTimeout(() => { displayPullRequestsForRepo(repoName) }, delay);
+  }
+}
 
 babbel
   .getRepos()
-  .then(repos => {
-    const repoArray = repos.data;
-    for (let i = 0; i < repoArray.length; i++) {
-      setTimeout( function timer(){
-        const repoName = repoArray[i].name;
-        let repo = gh.getRepo("lessonnine", repoName);
-        repo
-          .listPullRequests({})
-          .then(res => {
-            const numberOfOpenPullRequests = res.data.length;
-            if (numberOfOpenPullRequests != 0) {
-              for (let j = 0; j < numberOfOpenPullRequests; j++) {
-                const pullReq = res.data[j];
-                const pullRequestData = [
-                  res.headers.date,
-                  repoName,
-                  pullReq.html_url,
-                  pullReq.title,
-                  pullReq.created_at,
-                  pullReq.updated_at,
-                  pullReq.user.login
-                ];
-                console.log(pullRequestData.join(";"));
-              }
-            }
-          })
-          .catch(err => {
-             console.log("Forbidden");
-             process.exit(1);
-        });
-      }, i*10);
-    }
-  })
+  .then(displayPullRequestsForOrg)
   .catch(err => {
-    console.log(err);
+    console.log("Forbidden: getRepos() did not work.");
     process.exit(1);
   });
